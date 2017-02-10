f0b0b2b02a642d803fec69dd45790922171f7eac
Yvonne Tsu <yvonnnetsu@gmail.com>
2017-02-10 15:53:43 -0800

    added images and hardcoded profile info, and made a profile.js file

---
+++ models/site.js
@@ -0,0 +1,14 @@
+var mongoose = require('mongoose'),
+  Schema = mongoose.Schema;
+
+
+var SiteSchema = new Schema({
+  name: String,
+  location: String,
+  image: String,
+
+});
+
+var Site = mongoose.model('Site', SiteSchema);
+
+module.exports = Site;
